82744f372bc03be120cb7a2e89b85606850a86e5
suzukiken <suzukiken@gmail.com>
2021-03-25 01:05:29 +0000

    done

---
+++ lambda/index.py
@@ -0,0 +1,6 @@
+import time
+
+def handler(event, context):
+
+    print('event {}'.format(event))
+
--- lib/cdklambda-cloudwatch-stack.ts
+++ lib/cdklambda-cloudwatch-stack.ts
@@ -1,9 +1,32 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import { PythonFunction } from "@aws-cdk/aws-lambda-python";
+import * as logs from "@aws-cdk/aws-logs";
+import { LambdaDestination } from "@aws-cdk/aws-logs-destinations";
 
 export class CdklambdaCloudwatchStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    const PREFIX_NAME = id.toLocaleLowerCase().replace("stack","")
 
-    // The code that defines your stack goes here
+    const lambda_function = new PythonFunction(this, "lambda_function", {
+      entry: "lambda",
+      index: "index.py",
+      handler: "lambda_handler",
+      functionName: PREFIX_NAME,
+      runtime: lambda.Runtime.PYTHON_3_8,
+      timeout: cdk.Duration.seconds(10),
+    });
+    
+    const log_group = new logs.LogGroup(this, 'log_group', {
+      logGroupName: PREFIX_NAME
+    });
+
+    const subscription = new logs.SubscriptionFilter(this, 'subscription', {
+      logGroup: log_group,
+      destination: new LambdaDestination(lambda_function),
+      filterPattern: logs.FilterPattern.allTerms("ERROR")
+    });
   }
 }
